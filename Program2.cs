72bc01c296f52549d3204ec25f4e8fd881cb0e00
Aya2002Amjad <aya2002amjad@gmail.com>
2022-11-01 19:44:55 +0300

    addingQ2+3

---
+++ Program2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment1._2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the array size: ");
+            int n =Convert.ToInt32( Console.ReadLine());
+            int[] arr = new int[n];
+            for(int i=0; i<arr.Length; i++)
+            {
+                Console.WriteLine("Please enter num {0}{1}",i+1,":");
+                arr[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The output for the sorted array : ");
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first string: ");
+            string string1=Console.ReadLine();
+            Console.WriteLine("Please enter the second string: ");
+            string string2 = Console.ReadLine();
+            int size = string1.Length + string2.Length;
+            char [] newstring=new char[size] ;
+            
+            if (string1.Length == string2.Length)
+            {
+                int index = 0;
+                for (int i = 0; i < string1.Length; i++)
+                {
+                    newstring[index] = string1[i];
+                    index += 2;
+                }
+                   index = 1;
+                for (int i = 0; i < string2.Length; i++)
+                {
+                    newstring[index] = string2[i];
+                    index += 2;
+                }
+                Console.WriteLine(newstring);
+            }
+            else
+                Console.WriteLine(newstring);
+
+
+
+
+        }
+    }
+}
